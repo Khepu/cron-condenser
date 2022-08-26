4c87f8783172a587b409d01c887ee897c6d153ed
Khepu <makris.giwrgos1234@gmail.com>
2022-08-26 21:53:49 +0300

    Feature: Add cron-expression conversion methods

--- src/cron_condenser/cron_expression.clj
+++ src/cron_condenser/cron_expression.clj
@@ -1,4 +1,6 @@
 (ns cron-condenser.cron-expression
+  (:require
+   [clojure.string :as string])
   (:import
    [clojure.lang PersistentHashSet]))
 
@@ -9,3 +11,17 @@
      ^PersistentHashSet day
      ^PersistentHashSet month
      ^PersistentHashSet week-day])
+
+(defn ^CronExpression str->CronExpression
+  [^String cron-str]
+  (->> (string/split cron-str #" ")
+       (map hash-set)
+       (apply ->CronExpression)))
+
+(defn ^String CronExpression->str
+  [^CronExpression cron]
+  (str (string/join "," (:minute   cron)) " "
+       (string/join "," (:hour     cron)) " "
+       (string/join "," (:day      cron)) " "
+       (string/join "," (:month    cron)) " "
+       (string/join "," (:week-day cron))))
