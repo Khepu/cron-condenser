0e2ee5902119eaf4624d45ef8721db9abb565b1e
Khepu <makris.giwrgos1234@gmail.com>
2022-08-27 20:16:29 +0300

    Chore: Remove dead code

--- src/cron_condenser/planner.clj
+++ src/cron_condenser/planner.clj
@@ -1,19 +1,11 @@
 (ns cron-condenser.planner
   (:require
-   [clojure.set :refer [union intersection]]
+   [clojure.set :refer [union]]
    [cron-condenser.merger :refer [mergeable? merge-crons]])
   (:import
    [clojure.lang PersistentHashSet]
    [cron_condenser.cron_expression CronExpression]))
 
-(defn intersection-over-union
-  "Intersection over union is used to quantify how compatible 2 crons are. As
-  the score gets closer to one between two crons, they are considered a better
-  match to merge."
-  [^PersistentHashSet set-a ^PersistentHashSet set-b]
-  (let [ab-union (count (union set-a set-b))
-        ab-intersection (count (intersection set-a set-b))]
-    (/ ab-intersection ab-union)))
 
 (defn branch
   [^PersistentHashSet crons
@@ -29,23 +21,6 @@
   (->> crons
        (map #(branch crons %))
        (apply merge)))
-
-(defn longest
-  [a key-b matches-b]
-  (let [current-segment-length (count matches-b)]
-    (if (> current-segment-length (:length a))
-      {key-b matches-b
-       :length current-segment-length}
-      a)))
-
-;;TODO Calculate lengths before-hand
-(defn prune
-  "Keep only the longest 'branch' of valid merges."
-  [sub-graph]
-  (dissoc (reduce-kv longest
-                     {:length -1}
-                     sub-graph)
-          :length))
 
 (defn count-connections
   [connection-map]
